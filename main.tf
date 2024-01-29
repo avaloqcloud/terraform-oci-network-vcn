9d96709a9dd84b57321756d70d6709204e9b8ccb
Andreas Kaderli <andreas.kaderli@avaloq.com>
2024-01-29 09:02:53 +0100

    update 1

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
-# Your 'main.tf' code goes here.
+resource "oci_core_vcn" "this" {
+  compartment_id = var.vcn.compartment_id
 
-# You should create additional files if relevant for your project:
-# * 'outputs.tf' to declare your output variables
-# * 'data.tf' to declare your data sources
+  #TODO: BYOD IPv6 functionality not tested, in case this is needed further engineering required
+  dynamic "byoipv6cidr_details" {
+    for_each = (var.vcn.byoipv6cidr_details != null) ? var.vcn.byoipv6cidr_details : []
+    content {
+      byoipv6range_id = oci_core_byoipv6range.test_byoipv6range.id
+      ipv6cidr_block  = byoipv6cidr_details.ipv6cidr_block
+    }
+  }
+
+  cidr_blocks                      = var.vcn.cidr_blocks
+  defined_tags                     = var.vcn.defined_tags
+  display_name                     = var.vcn.display_name
+  dns_label                        = var.vcn.dns_label
+  freeform_tags                    = var.vcn.freeform_tags
+  ipv6private_cidr_blocks          = var.vcn.ipv6private_cidr_blocks
+  is_ipv6enabled                   = var.vcn.is_ipv6enabled
+  is_oracle_gua_allocation_enabled = var.vcn.is_oracle_gua_allocation_enabled
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,26 @@
-# Your 'variables.tf' input variables go here.
+variable "vcn" {
+  description = "VCN Input Object"
+  type = object({
+    compartment_id = string,
+    byoipv6cidr_details = optional(list(object({
+      #Required
+      byoipv6range_id = string,
+      ipv6cidr_block  = string
+    }))),
+    cidr_blocks                      = list(string),
+    defined_tags                     = optional(map(string)),
+    display_name                     = string,
+    dns_label                        = optional(string)
+    freeform_tags                    = optional(map(string)),
+    ipv6private_cidr_blocks          = optional(list(string)),
+    is_ipv6enabled                   = optional(bool),
+    is_oracle_gua_allocation_enabled = optional(bool)
+  })
+  # Validation
+  ## dns_label
+  validation {
+    condition     = var.vcn.dns_label != null ? can(regex("^[a-z](?:[a-z0-9]){1,15}$", var.vcn.dns_label)) : true
+    error_message = "Validation of the VCN object failed. 'dns_label' requirement max 15 alphanumeric characters and must start with a letter."
+  }
+}
+
